d4bb866dfff0ba912e818b0fe90aee9b8f40df45
Milan Slijepcevic <Milan_Slijepcevic@sluz.ch>
2025-09-09 16:10:41 +0200

    Aufgabe 1 Fertig

--- task_1_1.py
+++ task_1_1.py
@@ -1,5 +1,7 @@
-print("Hallo")
+name = input("Write name: ")
+print(f"Good to see you {name}")
 
-print("Das ist mein erstes Programm")
-
-print("DU!")
+zahl1 = input("write number one: ")
+zahl2 = input("write number two: ")
+summe = int(zahl1) + int(zahl2)
+print(f"The answer is {summe}")
