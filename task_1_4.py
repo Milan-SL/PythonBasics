ac5146155c5fe7ad400bec2043abf63706b7f055
Milan Slijepcevic <Milan_Slijepcevic@sluz.ch>
2025-09-16 15:55:46 +0200

    Aufgabe 4 fertig

---
+++ task_1_4.py
@@ -0,0 +1,12 @@
+numb1 =  input("Write first number:")
+numb2 = input("Write second number:")
+Summe = int(numb1) + int(numb2)
+Produkt = int(numb1) * int(numb2)
+Differenz = int(numb1) - int(numb2)
+Quotent = int(numb1) / int(numb2)
+print(f"Die Summe ist {round(Summe,2)}")
+print(f"Das Produkt ist {round(Produkt,2)}")
+print(f"Die Differenz ist {round(Differenz,2)}")
+print(f"Der Quotent ist {round(Quotent,2)}")
+
+#// = int division
