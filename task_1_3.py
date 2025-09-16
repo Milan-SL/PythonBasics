570b8d5b11dbf79b678d2f457dedd2b120aa9e99
Milan Slijepcevic <Milan_Slijepcevic@sluz.ch>
2025-09-16 15:31:25 +0200

    Aufgabe 3 fertig

---
+++ task_1_3.py
@@ -0,0 +1,9 @@
+name = "Milan"
+print(name.upper())
+print(name.lower())
+print(name[::-1]) #splicing = ::1
+
+wort1 = input("Write first word:")
+wort2 = input("Write second word:")
+summe = wort1 + wort2
+print(summe)
