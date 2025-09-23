841f31f2a5d9da24448870578ad99c47de4f862b
Milan Slijepcevic <Milan_Slijepcevic@sluz.ch>
2025-09-23 15:42:38 +0200

    Aufgabe 8 fertig

---
+++ task_1_8.py
@@ -0,0 +1,4 @@
+num = int(input("Write number:"))
+if num >= 10 and  num <= 20:    print (f" The number {num} is between 10 and 20.")
+elif  num <= 0 or num >= 100:
+    print(f"The number is smaller than 0 or bigger than 100.")
