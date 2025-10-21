c0265116315f4b63f858f0c007d2d9cd379ca1cf
Milan Slijepcevic <Milan_Slijepcevic@sluz.ch>
2025-10-21 16:04:31 +0200

    block 2 aufgabe 5 fertig

---
+++ block_2/task_2_5.py
@@ -0,0 +1,10 @@
+monat = input("Write a month:")
+match monat:
+    case "march"|"april"|"may":
+        print("it's spring")
+    case "june"|"july"|"august":
+        print("it's summer")
+    case "september"|"oktober"|"november":
+        print("it's autumn")
+    case "december"|"january"|"febuary":
+        print("it's winter")
